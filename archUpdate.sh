d6a434cda298f4f3538956fafe73aea6148f6f5a
nxs_neo <neo_abraham@proton.me>
2025-03-06 01:19:11 +0530

    Replace pacman and yay with Aura, added Pip

--- archUpdate.sh
+++ archUpdate.sh
@@ -6,165 +6,165 @@ B='\033[1;4;34m'
 Y='\033[1;4;33m'
 NORMAL='\033[0m'
 
-__pcmn () {
+__pip() {
 
 	echo
-	echo "${Y}Updating...${NORMAL}"
+	echo "${Y}Updating Python packages...${NORMAL}"
 	echo
-	if [ "$1" = "-ay" ];
-	then
-		sudo pacman -Syu --noconfirm
-	fi
-	if [ "$1" = "-a" ];
-	then
-		sudo pacman -Syu
+	if [ "$1" = "-ay" ]; then
+		pipx upgrade-all -q
+	elif [ "$1" = "-a" ]; then
+		pipx upgrade-all
 	else
-		sudo pacman -Syu --noconfirm
+		pipx upgrade-all -q
 	fi
-
 
 }
 
-__fltpk () {
+__fltpk() {
 
 	echo
-	echo "${B}Updating...${NORMAL}"
+	echo "${B}Updating Flatpak...${NORMAL}"
 	echo
-	if [ "$1" = "-ay" ];
-	then
+	if [ "$1" = "-ay" ]; then
 		flatpak -y update
-	fi
-	if [ "$1" = "-a" ];
-	then
+	elif [ "$1" = "-a" ]; then
 		flatpak update
 	else
 		flatpak -y update
 	fi
 }
-
-__aur () {
+__aur() {
 
 	echo
-	echo "${R}Updating...${NORMAL}"
+	echo "${R}Updating Pacman and AUR...${NORMAL}"
 	echo
-	if [ "$1" = "-ay" ];
-	then
-		aura -Syu --aur --noconfirm
-	fi
-	if [ "$1" = "-a" ];
-	then
-		aura -Syu --aur
+	if [ "$1" = "-ay" ]; then
+		aura -Syu --noconfirm
+	elif [ "$1" = "-a" ]; then
+		aura -Syu
 	else
-		aura -Syu --aur --noconfirm
+		aura -Syu --noconfirm
 	fi
 }
 
-update () {
+update() {
 
-
-	if [ -z "$1" ];
-	then
-		__pcmn
-		__fltpk
+	if [ -z "$1" ]; then
 		__aur
+		__fltpk
+		__pip
 	fi
 
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
-			-a|--ask)
-				echo "${Y}Do you want to update Pacman packages?${NORMAL}"
-				read -r yn
-				if (true) then
-					case $yn in
-						yes|Yes|YES|y|Y ) __pcmn "-a";;
-						no|NO|No|n|N ) ;;
-						* ) echo "${R}invalid response${NORMAL}" ;;
-					esac
-				fi
-
-				echo ""
-				echo "${B}Do you want to update Flatpak packages?${NORMAL}"
-				read -r yn
-				case $yn in
-					yes|Yes|YES|y|Y ) __fltpk "-a";;
-					no|NO|No|n|N ) ;;
-					* ) echo "${R}invalid response${NORMAL}" ;;
-				esac
-
-				echo ""
-				echo "${R}Do you want to update AUR packages?${NORMAL}"
-				read -r yn
-				case $yn in
-					yes|Yes|YES|y|Y ) __aur "-a"; return;;
-					no|NO|No|n|N ) return;;
-					* ) echo "${R}invalid response${NORMAL}" ;;
-				esac
-				echo "${G}DONE!!${NORMAL}"
-			;;
-
-
-			-ay)
-				echo "${Y}Do you want to update Pacman packages?${NORMAL}"
-				read -r yn
-				if (true) then
-					case $yn in
-						yes|Yes|YES|y|Y ) __pcmn "-ay";;
-						no|NO|No|n|N ) ;;
-						* ) echo "${R}invalid response${NORMAL}" ;;
-					esac
-				fi
-
-				echo ""
-				echo "${B}Do you want to update Flatpak packages?${NORMAL}"
-				read -r yn
-				case $yn in
-					yes|Yes|YES|y|Y ) __fltpk "-ay";;
-					no|NO|No|n|N ) ;;
-					* ) echo "${R}invalid response${NORMAL}" ;;
-				esac
-
-				echo ""
-				echo "${R}Do you want to update AUR packages?${NORMAL}"
-				read -r yn
-				case $yn in
-					yes|Yes|YES|y|Y ) __aur "-ay"; return;;
-					no|NO|No|n|N ) return;;
-					* ) echo "${R}invalid response${NORMAL}" ;;
-				esac
-				echo "${G}DONE!!${NORMAL}"
-			;;
-
-			-h|--help)
-				echo ""
-				echo "Arch Update Script written by Neo"
-				echo
-				echo "OPTIONS: "
-				echo "default behaviour: run updates for all package managers without asking"
-				echo "-a or --ask : ask what package managers to update for. "
-				echo "-h or --help : run this help dialogue"
-				echo "-s or --shutdown : update and shutdown"
-				echo "-r or --reboot or --restart : update and reboot"
-				echo
+		-a | --ask)
+			echo "${R}Do you want to update AUR packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y)
+				__aur "-a"
 				return
-			;;
+				;;
+			no | NO | No | n | N) return ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
 
-			-s|--shutdown)
-				__pcmn
-				__fltpk
-				__aur
-				shutdown now
-			;;
+			echo ""
+			echo "${B}Do you want to update Flatpak packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y) __fltpk "-a" ;;
+			no | NO | No | n | N) ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
 
-			-r|--reboot|--restart)
-				__pcmn
-				__fltpk
-				__aur
-				reboot
-			;;
-
-			*)
-				echo "Invalid Parameter"
+			echo ""
+			echo "${Y}Do you want to update Python packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y) __pip "-a" ;;
+			no | NO | No | n | N) ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
+			echo "${R}Do you want to update AUR packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y)
+				__aur "-ay"
 				return
+				;;
+			no | NO | No | n | N) return ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
+
+			echo "${G}DONE!!${NORMAL}"
+			;;
+
+		-ay)
+			echo "${R}Do you want to update AUR packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y)
+				__aur "-ay"
+				return
+				;;
+			no | NO | No | n | N) return ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
+
+			echo ""
+			echo "${B}Do you want to update Flatpak packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y) __fltpk "-ay" ;;
+			no | NO | No | n | N) ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
+
+			echo ""
+			echo "${Y}Do you want to update Python packages?${NORMAL}"
+			read -r yn
+			case $yn in
+			yes | Yes | YES | y | Y) __pip "-ay" ;;
+			no | NO | No | n | N) ;;
+			*) echo "${R}invalid response${NORMAL}" ;;
+			esac
+
+			echo "${G}DONE!!${NORMAL}"
+			;;
+
+		-h | --help)
+			echo ""
+			echo "Arch Update Script written by Neo"
+			echo
+			echo "OPTIONS: "
+			echo "default behaviour: run updates for all package managers without asking"
+			echo "-a or --ask : ask what package managers to update for. "
+			echo "-h or --help : run this help dialogue"
+			echo "-s or --shutdown : update and shutdown"
+			echo "-r or --reboot or --restart : update and reboot"
+			echo
+			return
+			;;
+
+		-s | --shutdown)
+			__pip
+			__fltpk
+			__aur
+			shutdown now
+			;;
+
+		-r | --reboot | --restart)
+			__pip
+			__fltpk
+			__aur
+			reboot
+			;;
+
+		*)
+			echo "Invalid Parameter"
+			return
 			;;
 		esac
 	done
